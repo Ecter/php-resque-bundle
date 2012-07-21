813fa009442ae974b55f4464f05afa8d55741311
Justin Rainbow <justin.rainbow@gmail.com>
2012-07-21 14:44:28 -0700

    PHP CS fixes

--- Command/QueueCommand.php
+++ Command/QueueCommand.php
@@ -10,22 +10,24 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 
 class QueueCommand extends ContainerAwareCommand
 {
-    protected function configure() {
-        parent::configure();
-        $this->setName('resque:queue')
-             ->setDescription('Attach a class into any specified queue to be performed.')
-             ->addArgument('job_class_namespace', InputArgument::REQUIRED, 'Resque Job Class name (with namespace)')
-             ->addArgument('queue_name', InputArgument::OPTIONAL, 'Queue name', 'default')
-             ->setHelp(<<<EOF
+    protected function configure()
+    {
+        $this
+            ->setName('resque:queue')
+            ->setDescription('Attach a class into any specified queue to be performed.')
+            ->addArgument('job_class_namespace', InputArgument::REQUIRED, 'Resque Job Class name (with namespace)')
+            ->addArgument('queue_name', InputArgument::OPTIONAL, 'Queue name', 'default')
+            ->setHelp(<<<EOF
 resque:queue Your\\ProjectBundle\\SubprojectBundle\\JobClass mailer
 This will put JobClass into 'mailer' queue
 
 You can enqueue new jobs using a PHPResqueBundle\Resque\Queue
 EOF
-);
+            );
     }
 
-    protected function execute(InputInterface $input, OutputInterface $output) {
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
         $job = Queue::add($input->getArgument('job_class_namespace'), $input->getArgument('queue_name'));
         $output->write("Job captured. Input at {$input->getArgument('queue_name')} queue. Job id {$job}");
     }
