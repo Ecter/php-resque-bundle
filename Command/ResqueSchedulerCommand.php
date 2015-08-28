ac6aa50fc7cc27a72d7050b82cfc822504585cb2
Philipp Gruber <philipp.gruber@catchoftheday.com.au>
2015-08-28 15:32:29 +1000

    adding php-resque-scheduler

---
+++ Command/ResqueSchedulerCommand.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PHPResqueBundle\Command;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+
+
+class ResqueSchedulerCommand extends ContainerAwareCommand
+{
+    protected function configure() {
+        $this->setName('resque:scheduler')
+             ->setDescription('Queues scheduled resque jobs')
+             ->addOption('log', 'l', InputOption::VALUE_OPTIONAL, 'Verbose mode [verbose|normal|none]')
+             ->addOption('interval', 'i', InputOption::VALUE_OPTIONAL, 'Daemon check interval (in seconds)', 5)
+        ;
+    }
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $worker = new \ResqueScheduler_Worker();
+        $worker->logLevel = $input->getOption('log');
+        $worker->work($input->getOption('interval'));
+    }
+}
